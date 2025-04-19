814b0cb3b6727eac8c971b605db8b1bf5402db36
Gurjot Singh <49863917+gurjotsinghdev@users.noreply.github.com>
2025-04-19 11:56:04 -0700

    new

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
+  title: "Rashmeen Malhotra",
   name: "Sirius",
   ogImage: "/opengraph-image.png",
   description:
